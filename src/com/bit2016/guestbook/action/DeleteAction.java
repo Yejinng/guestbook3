42ed9d887c4f858d4f0e3e363a0e01561486ff1b
BIT-USER <BIT-USER@BIT>
2016-10-17 17:51:06 +0900

    11

---
+++ src/com/bit2016/guestbook/action/DeleteAction.java
@@ -0,0 +1,30 @@
+package com.bit2016.guestbook.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bit2016.guestbook.dao.GuestbookDao;
+import com.bit2016.guestbook.vo.GuestbookVo;
+import com.bit2016.web.Action;
+
+public class DeleteAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String no = request.getParameter("no");
+		String password = request.getParameter("password");
+
+		GuestbookVo vo = new GuestbookVo();
+		vo.setNo(Long.parseLong(no));
+		vo.setPassword(password);
+		
+		GuestbookDao dao = new GuestbookDao();
+		dao.delete(vo);
+		
+		response.sendRedirect("/guestbook2/gs");
+	}
+
+}
